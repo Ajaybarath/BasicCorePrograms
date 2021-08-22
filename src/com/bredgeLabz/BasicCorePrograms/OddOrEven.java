9dfdf357f50bbe2594feedb86f0f80ac1c05ceaa
ajaybarath <ajaybarath3@gmail.com>
2021-08-22 19:59:45 +0530

    ADDED: odd or even number program added

---
+++ src/com/bredgeLabz/BasicCorePrograms/OddOrEven.java
@@ -0,0 +1,16 @@
+package com.bredgeLabz.BasicCorePrograms;
+
+class OddOrEven {
+	
+	public static void main(String args[]) {
+		int number = 2;
+		
+		if (number % 2 == 0) {
+			System.out.println(number + " is even number");
+		}
+		else {
+			System.out.println(number + " is odd number");
+		}
+	}
+
+}
