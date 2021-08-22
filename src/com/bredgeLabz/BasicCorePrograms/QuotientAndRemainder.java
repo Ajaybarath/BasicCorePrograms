4e7c3ae96df21a94f7ae96c016150f47706266cd
ajaybarath <ajaybarath3@gmail.com>
2021-08-22 20:09:29 +0530

    ADDED: quotient and remainder program added

---
+++ src/com/bredgeLabz/BasicCorePrograms/QuotientAndRemainder.java
@@ -0,0 +1,17 @@
+package com.bredgeLabz.BasicCorePrograms;
+
+public class QuotientAndRemainder {
+	
+	public static void main(String[] args) {
+		
+        int number1 = 15;
+        int number2 = 2;
+        
+        int quotient = number1 / number2;
+        int remainder = number1 % number2;
+        
+        System.out.println("Quotient is: " + quotient);
+        System.out.println("Remainder is: " + remainder);
+    }
+
+}
