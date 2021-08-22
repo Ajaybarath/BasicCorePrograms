458dc379c1f8eecc5a7b2a48dc614420cf498010
ajaybarath <ajaybarath3@gmail.com>
2021-08-22 19:30:45 +0530

    ADDED: flip coin program added

--- src/com/bredgeLabz/BasicCorePrograms/FlipCoin.java
+++ src/com/bredgeLabz/BasicCorePrograms/FlipCoin.java
@@ -3,28 +3,28 @@ package com.bredgeLabz.BasicCorePrograms;
 import java.util.Scanner;
 
 public class FlipCoin {
-	
+
 	public static void main(String args[]) {
-		
+
 		int numberOfHeads = 0;
 		int numberOfFlips = 0;
-		
+
 		Scanner s = new Scanner(System.in);
 		numberOfFlips = s.nextInt();
-		
-		for (int i=0;i<numberOfFlips;i++) {
+
+		for (int i = 0; i < numberOfFlips; i++) {
 			double coin = (double) Math.random();
-			System.out.println(coin);
+
 			if (coin > 0.5) {
 				numberOfHeads++;
 			}
 		}
-		
-		int headPercentage = ((numberOfHeads*100)/numberOfFlips);
+
+		int headPercentage = ((numberOfHeads * 100) / numberOfFlips);
 		int tailPercentage = 100 - headPercentage;
-		
+
 		System.out.println("Head percentage is " + headPercentage);
-		
+
 		System.out.println("Tail percentage is " + tailPercentage);
 
 	}
