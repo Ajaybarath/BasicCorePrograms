a0267036f9130321fbc03314ee850cdcfa19dc2d
ajaybarath <ajaybarath3@gmail.com>
2021-08-22 19:08:30 +0530

    CREATED: head tail program

--- src/com/bredgeLabz/BasicCorePrograms/FlipCoin.java
+++ src/com/bredgeLabz/BasicCorePrograms/FlipCoin.java
@@ -1,9 +1,32 @@
 package com.bredgeLabz.BasicCorePrograms;
 
+import java.util.Scanner;
+
 public class FlipCoin {
 	
 	public static void main(String args[]) {
-		System.out.println("Hello");
+		
+		int numberOfHeads = 0;
+		int numberOfFlips = 0;
+		
+		Scanner s = new Scanner(System.in);
+		numberOfFlips = s.nextInt();
+		
+		for (int i=0;i<numberOfFlips;i++) {
+			double coin = (double) Math.random();
+			System.out.println(coin);
+			if (coin > 0.5) {
+				numberOfHeads++;
+			}
+		}
+		
+		int headPercentage = ((numberOfHeads*100)/numberOfFlips);
+		int tailPercentage = 100 - headPercentage;
+		
+		System.out.println("Head percentage is " + headPercentage);
+		
+		System.out.println("Tail percentage is " + tailPercentage);
+
 	}
 
 }
